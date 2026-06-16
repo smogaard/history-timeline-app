be763cf13526a654096e19f72f31a35a7f6d6ca5
smogaard <smogaard@gmail.com>
2026-06-16 23:41:30 +0200

    add visibility to initial timelines

--- src/data/events.ts
+++ src/data/events.ts
@@ -5,6 +5,7 @@ export const timelines: Timeline[] = [
     id: "periods",
     title: "Historiske perioder",
     type: "periods",
+    visibility: "public",
     periods: [
       {
         id: "p1",
@@ -25,6 +26,7 @@ export const timelines: Timeline[] = [
     id: "world",
     title: "Verdenshistorie",
     type: "events",
+    visibility: "public",
     events: [
       { id: "1", title: "Den franske revolusjon", year: 1789 },
       { id: "2", title: "1. verdenskrig", year: 1914 },
@@ -36,6 +38,7 @@ export const timelines: Timeline[] = [
     id: "norway",
     title: "Norge",
     type: "events",
+    visibility: "public",
     events: [
       { id: "4", title: "Grunnloven", year: 1814 },
       { id: "5", title: "Union med Sverige avsluttes", year: 1905 },
